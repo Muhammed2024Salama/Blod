8b0a952909d48ccfcf7d0822848e14f2e3f5e7c7
devmuhammedsalama <devmuhammedsalama@gmail.com>
2024-11-23 14:09:38 +0200

    Refactor Repository and Make Interface Command

--- app/Console/Commands/MakeInterfaceCommand.php
+++ app/Console/Commands/MakeInterfaceCommand.php
@@ -5,22 +5,21 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Filesystem\Filesystem;
 
-
-class MakeRepositoryCommand extends Command
+class MakeInterfaceCommand extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'make:repository {name : The name of the repository}';
+    protected $signature = 'make:interface {name : The name of the interface}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Create a new repository file';
+    protected $description = 'Create a new interface file';
 
     /**
      * Execute the console command.
@@ -32,25 +31,25 @@ class MakeRepositoryCommand extends Command
         $name = $this->argument('name');
         $filesystem = app(Filesystem::class);
 
-        // Path for the new repository
-        $path = app_path("Repository/{$name}.php");
+        // Path for the new interface
+        $path = app_path("Interface/{$name}.php");
 
         // Check if file already exists
         if ($filesystem->exists($path)) {
-            $this->error("Repository {$name} already exists!");
+            $this->error("Interface {$name} already exists!");
             return Command::FAILURE;
         }
 
         // Ensure the directory exists
-        $filesystem->ensureDirectoryExists(app_path('Repository'));
+        $filesystem->ensureDirectoryExists(app_path('Interface'));
 
         // Stub content
         $stub = <<<PHP
         <?php
 
-        namespace App\Repository;
+        namespace App\Interface;
 
-        class {$name}
+        interface {$name}
         {
             //
         }
@@ -59,7 +58,7 @@ class MakeRepositoryCommand extends Command
         // Write the file
         $filesystem->put($path, $stub);
 
-        $this->info("Repository {$name} created successfully!");
+        $this->info("Interface {$name} created successfully!");
         return Command::SUCCESS;
     }
 }
