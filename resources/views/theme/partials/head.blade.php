183bd522dcbeb72e83dce3411d0e36416d1be87f
devmuhammedsalama <devmuhammedsalama@gmail.com>
2024-11-23 11:52:49 +0200

    add theme to view modules

---
+++ resources/views/theme/partials/head.blade.php
@@ -0,0 +1,16 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog - @yield('title')</title>
+    <link rel="icon" href="{{ asset('frontend/assets/img/Fevicon.png') }}" type="image/png">
+
+    <link rel="stylesheet" href="{{ asset('frontend/assets/vendors/bootstrap/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/assets/vendors/fontawesome/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/assets/vendors/themify-icons/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/assets/vendors/linericon/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/assets/vendors/owl-carousel/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/assets/vendors/owl-carousel/owl.carousel.min.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/style.css') }}">
+</head>
---
+++ resources/views/theme/partials/scripts.blade.php
@@ -0,0 +1,6 @@
+<script src="{{ asset('frontend/assets/vendors/jquery/jquery-3.2.1.min.js') }}"></script>
+<script src="{{ asset('frontend/assets/vendors/bootstrap/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('frontend/assets/vendors/owl-carousel/owl.carousel.min.js') }}"></script>
+<script src="{{ asset('frontend/assets/js/jquery.ajaxchimp.min.js') }}"></script>
+<script src="{{ asset('frontend/assets/js/mail-script.js') }}"></script>
+<script src="{{ asset('frontend/assets/js/main.js') }}"></script>
